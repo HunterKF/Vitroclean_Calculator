0bbc39b7c70695299d403ad4f237820eb4b39ab2
HunterKF <95839328+HunterKF@users.noreply.github.com>
2024-03-27 07:53:27 +0700

    Changed email

--- shared/src/commonMain/kotlin/com/jaegerapps/vitroclean/shared/data/remote/dtos/PoolFilterDto.kt
+++ shared/src/commonMain/kotlin/com/jaegerapps/vitroclean/shared/data/remote/dtos/PoolFilterDto.kt
@@ -1,13 +1,15 @@
 package com.jaegerapps.vitroclean.shared.data.remote.dtos
 
+import kotlinx.serialization.SerialName
+
 @kotlinx.serialization.Serializable
 data class PoolFilterDto(
-    val id: Int,
-    val manufacturer_name: String,
-    val model: String,
-    val recommended_vitroclean_vfa_load_lbs: Int,
-    val recommended_sand_load_lbs: Int,
-    val recommended_vitroclean_pebble_vfeight_load_lbs: Int,
-    val fifty_lb_bags_of_vitroclean_vfa : Int,
-    val fifty_lb_bags_of_pebble_vfeight : Int,
+    @SerialName("id")val id: Int,
+    @SerialName("manufacturer_name")val manufacturer_name: String,
+    @SerialName("model")val model: String,
+    @SerialName("recommended_sand_load_lbs")val recommended_sand_load_lbs: Int,
+    @SerialName("recommended_vitroclean_load_lbs")val recommended_vitroclean_vfa_load_lbs: Int,
+    @SerialName("recommended_vitroclean_pebble_load_lbs")val recommended_vitroclean_pebble_vfeight_load_lbs: Int,
+    @SerialName("fifty_lb_bags_of_vitroclean")val fifty_lb_bags_of_vitroclean_vfa : Int,
+    @SerialName("fifty_lb_bags_of_vitroclean_pebble")val fifty_lb_bags_of_pebble_vfeight : Int,
 )
