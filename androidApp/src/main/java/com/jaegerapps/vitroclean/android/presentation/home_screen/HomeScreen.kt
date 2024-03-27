85bbed637fd3240a8615a02a20b45846664d310d
HunterKF <95839328+HunterKF@users.noreply.github.com>
2024-03-27 08:11:25 +0700

    UI Adjustments

--- androidApp/src/main/java/com/jaegerapps/vitroclean/android/presentation/home_screen/HomeScreen.kt
+++ androidApp/src/main/java/com/jaegerapps/vitroclean/android/presentation/home_screen/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.jaegerapps.vitroclean.android.presentation.home_screen
 import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
@@ -19,7 +20,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -104,6 +107,13 @@ fun HomeScreen(
         ) {
             item {
                 Spacer(Modifier.height(16.dp))
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f),
+                    painter = painterResource(id = com.jaegerapps.vitroclean.R.drawable.logo_vitroclean),
+                    contentScale = ContentScale.Fit,
+                    contentDescription = null
+                )
             }
             item {
 
